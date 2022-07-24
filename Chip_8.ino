a0d8f097c9589da365b3585a374aad1a83aef65a
T7F4 <68499962+jansaternus@users.noreply.github.com>
2022-07-24 16:23:05 +0200

    Update Chip_8.ino
    Ability to read .ch8 files and displaying them byte by byte in the RAM memory at the display.

--- Chip_8.ino
+++ Chip_8.ino
@@ -1,11 +1,14 @@
 #include <MCUFRIEND_kbv.h>
 MCUFRIEND_kbv tft;       // hard-wired for UNO shields anyway.
 #include <TouchScreen.h>
+#include <SPI.h>
+#include <SD.h>
 
 const int TS_LEFT=907,TS_RT=136,TS_TOP=942,TS_BOT=139;
 
 TouchScreen ts = TouchScreen(6, A1, A2, 7, 300);
 TSPoint tp;
+File myFile;
 
 #define MINPRESSURE 200
 #define MAXPRESSURE 1000
@@ -25,6 +28,7 @@ TSPoint tp;
 #define RAM_HEIGHT 256
 
 void setup() {
+  // Init display and serial
   Serial.begin(9600);
   tft.reset();
   uint16_t id = tft.readID();
@@ -32,10 +36,36 @@ void setup() {
   tft.setRotation(1);
   tft.fillScreen(BLACK);
 
-  
-  tft.fillRect(100, 100, 1, 1, 0x5F36);
-  ramByteWrite(0x200, 0x4356);
-  Serial.println(ramByteRead(0x200), HEX);
+  // init SD card
+  Serial.print("Initializing SD card...");
+  pinMode(10, OUTPUT);
+
+  initMemory();
+
+  int i = 0;
+
+  if (!SD.begin(10)) {
+    Serial.println("initialization failed!");
+    return;
+  }
+  Serial.println("initialization done.");
+
+  myFile = SD.open("IBM_Logo.ch8");
+  if (myFile) {
+    Serial.println("IBM_Logo.ch8:");
+    
+    // read from the file until there's nothing else in it:
+    while (myFile.available()) {
+      Serial.println(myFile.read());
+      ramByteWrite(0x200+i, myFile.read());
+      i++;
+    }
+    // close the file:
+    myFile.close();
+  } else {
+  	// if the file didn't open, print an error:
+    Serial.println("error opening IBM_Logo.ch8");
+  }
   
 
 }
@@ -56,4 +86,11 @@ void ramByteWrite(int addr, short value){
  int x = RAM_POS_X + addr%RAM_WIDTH;
  int y = RAM_POS_Y + round(addr/RAM_WIDTH);
  tft.fillRect(x,y,1,1,value);
+}
+
+void initMemory(){
+  tft.fillRect(RAM_POS_X - 1, RAM_POS_Y - 1, RAM_WIDTH + 2, RAM_HEIGHT + 2, WHITE);
+  for(int i = 0; i < 4096; i++){
+    ramByteWrite(i, 0);
+  }
 }
